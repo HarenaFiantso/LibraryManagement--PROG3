141f98a3a695f9342a3298a63e817de3c584f27f
HarenaFiantso <fiantsorav@gmail.com>
2023-11-19 16:32:05 +0300

    refactor: Make some changes for the create table author sql script

---
+++ src/main/resources/create_table_author.sql
@@ -0,0 +1,12 @@
+-- Create the table author
+CREATE TABLE author (
+    authorId SERIAL PRIMARY KEY,
+    authorName VARCHAR(255) NOT NULL,
+    sex CHAR(1) CHECK (sex IN ('M', 'F')) NOT NULL
+);
+
+-- Insert data for the table author
+INSERT INTO author (authorName, sex) VALUES
+('Author 1', 'M'),
+('Author 2', 'F'),
+('Author 3', 'M');
