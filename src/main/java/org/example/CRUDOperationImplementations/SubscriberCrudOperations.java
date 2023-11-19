829340104c315fa0bc451b1d4b334d022c6cf6a9
HarenaFiantso <fiantsorav@gmail.com>
2023-11-19 19:22:39 +0300

    feat: Create a new java class for the subscribers crud operation

---
+++ src/main/java/org/example/CRUDOperationImplementations/SubscriberCrudOperations.java
@@ -0,0 +1,28 @@
+package org.example.CRUDOperationImplementations;
+
+import org.example.CRUDOperations;
+import org.example.entity.Subscriber;
+
+import java.util.List;
+
+public class SubscriberCrudOperations implements CRUDOperations<Subscriber> {
+    @Override
+    public List<Subscriber> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<Subscriber> saveAll(List<Subscriber> toSave) {
+        return null;
+    }
+
+    @Override
+    public Subscriber save(Subscriber toSave) {
+        return null;
+    }
+
+    @Override
+    public Subscriber delete(Subscriber toDelete) {
+        return null;
+    }
+}
