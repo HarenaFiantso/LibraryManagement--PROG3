f3ba8e131732427f7a7c830b495bb7acd85b4a8a
HarenaFiantso <fiantsorav@gmail.com>
2023-11-19 14:33:25 +0300

    feat: Create a new java class for the subscriber entity

---
+++ src/main/java/org/example/entity/Subscriber.java
@@ -0,0 +1,16 @@
+package org.example.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor(force = true)
+public class Subscriber extends User{
+    private LocalDate subscriptionDate;
+}
