ebf35a7897743bcd80e7f9f22f5297f613b8b3da
HarenaFiantso <fiantsorav@gmail.com>
2023-11-19 22:47:27 +0300

    test: Make some test with the Book entity

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,11 @@
 package org.example;
 
 import org.example.test.AuthorTest;
+import org.example.test.BookTest;
 
 public class Main {
     public static void main(String[] args) {
         AuthorTest.launch();
+        BookTest.launch();
     }
 }
---
+++ src/main/java/org/example/test/BookTest.java
@@ -0,0 +1,22 @@
+package org.example.test;
+
+import org.example.crudOperationImplementations.BookCrudOperations;
+import org.example.entity.Book;
+import org.example.entity.enumeration.Topic;
+
+import java.sql.Date;
+import java.util.List;
+
+public class BookTest {
+    private final static BookCrudOperations bookCrudOperations = new BookCrudOperations();
+
+    public static void launch() {
+        Utils.printList(bookCrudOperations.findAll());
+
+        System.out.println(bookCrudOperations.save(new Book("", "Book 4", 200, Date.valueOf("2023-01-01"), List.of(Topic.COMEDY), null)));
+
+        Utils.printList(bookCrudOperations.saveAll(List.of(new Book("", "Book 5", 200, Date.valueOf("2023-02-01"), List.of(Topic.ROMANCE, Topic.OTHER), null), new Book("", "bookName_6", 200, Date.valueOf("2023-03-01"), List.of(Topic.OTHER), null))));
+
+        Utils.printList(bookCrudOperations.findAll());
+    }
+}
