77c14903b231c313adb7765340d6443299d5d955
HarenaFiantso <fiantsorav@gmail.com>
2023-11-19 12:21:40 +0300

    chore: Make a simple test connection between the database and the java main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String jdbcUrl = "jdbc:postgresql://localhost:5432/library_management";
+        String user = "prog_admin";
+        String password = "123456";
+
+        try (Connection connection = DriverManager.getConnection(jdbcUrl, user, password)) {
+            if (connection != null) {
+                System.out.println("Database connected ! ");
+            }
+        } catch (SQLException e) {
+            System.out.println("Database connection error : " + e.getMessage());
+        }
     }
 }
