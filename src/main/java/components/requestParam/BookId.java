5302df4e8af3925b8d740fa615ea00b63e5980f7
HarenaFiantso <fiantsorav@gmail.com>
2023-11-19 13:56:05 +0300

    feat: Create a new java class for the book Id request parameters

---
+++ src/main/java/components/requestParam/BookId.java
@@ -0,0 +1,12 @@
+package components.requestParam;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor(force = true)
+public class BookId {
+    private String bId;
+}
