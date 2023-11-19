3fb69139e3545d69b6fe699dc73b82624d21b089
HarenaFiantso <fiantsorav@gmail.com>
2023-11-19 13:51:43 +0300

    feat: Create a new java class to store the path pagination component

--- src/main/java/components/requestParam/PathPagination.java
+++ src/main/java/components/requestParam/PathPagination.java
@@ -1,4 +1,12 @@
 package components.requestParam;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor(force = true)
 public class PathPagination {
+    private int page;
 }
