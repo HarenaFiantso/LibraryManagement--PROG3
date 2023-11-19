c4708fed45dcdbfddf30ce750e97520ad7e3c0b1
HarenaFiantso <fiantsorav@gmail.com>
2023-11-19 16:07:01 +0300

    fix: Fix the connection between the database connection java class and the main java class file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,8 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 public class Main {
     public static void main(String[] args) {
-        String jdbcUrl = System.getenv("JDBC_URL");
-        String user = System.getenv("DB_USER");
-        String password = System.getenv("DB_PASSWORD");
-
-        System.out.println("JDBC_URL: " + jdbcUrl);
-        System.out.println("DB_USER: " + user);
-        System.out.println("DB_PASSWORD: " + password);
-
-        try (Connection connection = DriverManager.getConnection(jdbcUrl, user, password)) {
-            if (connection != null) {
-                System.out.println("Database connected ! ");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        DatabaseConnection.getConnection();
+        DatabaseConnection.closeConnection();
     }
 }
