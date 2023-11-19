a6a27e95e76169589ed09c940544bd7b1e190576
HarenaFiantso <fiantsorav@gmail.com>
2023-11-19 20:02:01 +0300

    refactor: Reformat the AuthorCrudOperations

--- src/main/java/org/example/CRUDOperationImplementations/AuthorCrudOperations.java
+++ src/main/java/org/example/CRUDOperationImplementations/AuthorCrudOperations.java
@@ -10,23 +10,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AuthorCrudOperations implements CRUDOperations<Author> {
+    private static final String SELECT_ALL_QUERY = "SELECT * FROM author";
+    private static final String INSERT_QUERY = "INSERT INTO author (author_name, sex) VALUES (?, ?)";
+    private static final String DELETE_QUERY = "DELETE FROM author WHERE author_id = ?";
+
     private static final Connection connection = DatabaseConnection.getConnection();
 
     public static Author authorInstance(ResultSet resultSet) throws SQLException {
-        return new Author(
-                resultSet.getLong("author_id"),
-                resultSet.getString("author_name"),
-                Sex.valueOf(resultSet.getString("sex"))
-        );
+        return new Author(resultSet.getLong("author_id"), resultSet.getString("author_name"), Sex.valueOf(resultSet.getString("sex")));
     }
 
     @Override
     public List<Author> findAll() {
-        String SELECT_ALL_QUERY = "SELECT * FROM author";
         List<Author> authors = new ArrayList<>();
 
-        try {
-            ResultSet resultSet = connection.prepareStatement(SELECT_ALL_QUERY).executeQuery();
+        try (PreparedStatement statement = connection.prepareStatement(SELECT_ALL_QUERY); ResultSet resultSet = statement.executeQuery()) {
             while (resultSet.next()) {
                 authors.add(authorInstance(resultSet));
             }
@@ -45,18 +43,16 @@ public class AuthorCrudOperations implements CRUDOperations<Author> {
 
     @Override
     public Author save(Author toSave) {
-        String INSERT_QUERY = "INSERT INTO author (author_name, sex) VALUES (?, ?);";
         Author author = null;
 
-        try {
-            PreparedStatement statement = connection.prepareStatement(INSERT_QUERY, Statement.RETURN_GENERATED_KEYS);
+        try (PreparedStatement statement = connection.prepareStatement(INSERT_QUERY, Statement.RETURN_GENERATED_KEYS)) {
             statement.setString(1, toSave.getAuthorName());
             statement.setString(2, toSave.getSex().toString());
             statement.executeUpdate();
 
             ResultSet resultSet = statement.getGeneratedKeys();
             if (resultSet.next()) {
-                toSave.setAuthorId(resultSet.getInt(1));
+                toSave.setAuthorId(resultSet.getLong(1));
                 author = toSave;
             }
         } catch (SQLException e) {
@@ -67,19 +63,12 @@ public class AuthorCrudOperations implements CRUDOperations<Author> {
 
     @Override
     public Author delete(Author toDelete) {
-        String DELETE_QUERY = "DELETE FROM author WHERE author_id = ?;";
         Author author = null;
 
-        try {
-            PreparedStatement statement = connection.prepareStatement(DELETE_QUERY);
+        try (PreparedStatement statement = connection.prepareStatement(DELETE_QUERY)) {
             statement.setLong(1, toDelete.getAuthorId());
             statement.executeUpdate();
-
-            ResultSet resultSet = statement.getGeneratedKeys();
-            if (resultSet.next()) {
-                toDelete.setAuthorId(Long.parseLong(resultSet.getString(1)));
-                author = toDelete;
-            }
+            author = toDelete;
         } catch (SQLException e) {
             e.printStackTrace();
         }
