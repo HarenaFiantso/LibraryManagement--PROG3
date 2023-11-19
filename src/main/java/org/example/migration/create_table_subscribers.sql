ad7496cb6ddd1305b0b31e7e3b3a152982cb764a
HarenaFiantso <fiantsorav@gmail.com>
2023-11-19 15:14:10 +0300

    feat: Create a new sql script for the visitor subscribers

---
+++ src/main/java/org/example/migration/create_table_subscribers.sql
@@ -0,0 +1,15 @@
+-- Create a new table subscribers
+CREATE TABLE subscribers (
+    subscriber_id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    subscription_date DATE,
+    subscription_type VARCHAR(50)
+);
+
+-- Add three lines of data by default
+INSERT INTO subscribers (username, password, email, subscription_date, subscription_type) VALUES
+('User 1', 'password 1', 'user1@hei.school', '2023-01-01', 'premium'),
+('User 2', 'password 2', 'user2@hei.school', '2023-02-01', 'premium'),
+('User 3', 'password 3', 'user3@hei.school', '2023-03-01', 'premium');
